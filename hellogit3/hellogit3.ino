3c68046d33ba56bc6d3d505d34df29e2be6d5f7e
TeknoRoman <teknoroman@upainstitutua.org>
2025-06-30 18:03:37 +0200

    hellogit3 se le cambia el pin a 5

--- hellogit3/hellogit3.ino
+++ hellogit3/hellogit3.ino
@@ -1,4 +1,4 @@
-int Led = 3;
+int Led = 5;
 
 void setup()
 {
@@ -9,8 +9,8 @@ void loop()
 {
   digitalWrite (Led, HIGH);
   Serial.println("piztu");
-  delay (3000);
+  delay (5000);
   digitalWrite (Led, LOW);
   Serial.println("itzali");
-  delay (3000);
+  delay (5000);
 }
