2700807de0826af1c1d968ae226da8da63a1d710
TeknoRoman <teknoroman@upainstitutua.org>
2025-07-01 00:41:38 +0200

    ya está?

--- helloRama11/helloRama11.ino
+++ helloRama11/helloRama11.ino
@@ -7,13 +7,8 @@ void setup()
 void loop()
 {
   digitalWrite (Led, HIGH);
-<<<<<<< HEAD
   delay (6000);
   digitalWrite (Led, LOW);
   delay (6000);
-=======
-  delay (3000);
-  digitalWrite (Led, LOW);
-  delay (3000);
->>>>>>> rama1
+
 }
